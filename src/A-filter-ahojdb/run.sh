040bde38cd316a269b79c7cb48b63443cc961d08
skrhakv <v.skrhak@gmail.com>
2025-04-25 11:24:31 +0200

    configure the bash script

--- src/A-filter-ahojdb/run.sh
+++ src/A-filter-ahojdb/run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # constants
-readonly PYTHON_PATH=/home/vit/miniconda3/bin/activate
-readonly OUTPUT_PATH=/home/vit/Projects/cryptobench/data/A-filter-ahojdb-v2
+readonly PYTHON_PATH=/home/skrhakv/CryptoBench/holo_venv/bin/activate
+readonly OUTPUT_PATH=/home/skrhakv/CryptoBench/data/A-filter-ahojdb/holo_only_data
 # CAUTION: if you have multiple folders (i.e. I got 3 folders - storage1, storage2, storage3),
 # you need to run this for each of these folders separately 
-readonly INPUT_PATH=/home/vit/Projects/cryptobench/data/ahoj-db/storage3/storage/praha1/home/davidhoksza/projects/ahoj/output4
+readonly INPUT_PATH=/home/skrhakv/CryptoBench/data/ahoj-db/ahojdb_v2c/data
 
 # vars
 BASEDIR=$(realpath $(dirname $0))
@@ -13,24 +13,20 @@ TMP_DIR=$BASEDIR/tmp-data
 
 # deactivate old venv and activate venv
 deactivate
+
 source $PYTHON_PATH
 mkdir $TMP_DIR
 
 for batch_path in $INPUT_PATH/*/; do
     BATCH=${batch_path: -4:-1}
-    mkdir $TMP_DIR/$BATCH
 
-    for f in $INPUT_PATH/$BATCH/*.tgz; do tar -xvf "$f" -C $TMP_DIR/$BATCH --one-top-level > /dev/null; done
-
-    # remove incomplete pairs
     mkdir $TMP_DIR/$BATCH-pairs
 
-    for directory in $TMP_DIR/$BATCH/*/; do
+    for directory in $INPUT_PATH/$BATCH/*/; do
         cd $directory
         if ! [ -f "apo_filtered_sorted_results.csv" ]; then
             continue
         fi
-
         if test "$(wc -l < apo_filtered_sorted_results.csv)" -gt 1; then 
             cp -r $directory $TMP_DIR/$BATCH-pairs
         fi
@@ -41,7 +37,7 @@ for batch_path in $INPUT_PATH/*/; do
     # make pairs
     cd $BASEDIR
     python3 main.py $BATCH $TMP_DIR $OUTPUT_PATH 2>> error.txt
-
     rm -rf $TMP_DIR/$BATCH-pairs
+    break
 done
 
